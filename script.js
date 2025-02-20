4907669838875927c7749e93d9323f8316a2a536
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-20 23:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
+let order = document.querySelector("#infi-list");
+function addListItems(count) {
+    let currentLength = order.children.length; 
 
+    for (let i = 0; i < count; i++) {
+        let newli = document.createElement("li");
+        newli.textContent = `Item ${currentLength + i + 1}`;
+        order.appendChild(newli);
+    }
+}
+function isAtBottom() {
+    return order.scrollTop + order.clientHeight >= order.scrollHeight - 1; 
+
+}
+order.addEventListener("scroll", function () {
+    if (isAtBottom()) {
+        addListItems(2); 
+    }
+
+});
+    
+addListItems(10);
